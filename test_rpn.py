e052c621ed2d1e883aa4dc4514b9d772be4d3c71
Anthony L Tung <altung@caen-vnc-vm11.engin.umich.edu>
2016-10-07 15:09:48 -0400

    yes

--- test_rpn.py
+++ test_rpn.py
@@ -4,3 +4,12 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
+    def test_subtract(self):
+    	result = rpn.calculate("1 1 -")
+    	self.assertEqual(0, result)
+    def test_multiply(self):
+    	result = rpn.calculate("1 2 *")
+    	self.assertEqual(2, result)
+    def test_divide(self):
+    	result  rpn.calculate("2 2 /")
+    	self.assertEqual(1, result)
